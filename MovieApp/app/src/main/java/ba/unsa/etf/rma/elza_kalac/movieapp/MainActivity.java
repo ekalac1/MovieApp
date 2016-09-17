468d60858925a7cb2a5c4512c7898ee28e338443
ekalac1 <elzaa_95@hotmail.com>
2016-09-17 11:38:05 +0200

    Slide menu

--- MovieApp/app/src/main/java/ba/unsa/etf/rma/elza_kalac/movieapp/MainActivity.java
+++ MovieApp/app/src/main/java/ba/unsa/etf/rma/elza_kalac/movieapp/MainActivity.java
@@ -1,13 +1,41 @@
 package ba.unsa.etf.rma.elza_kalac.movieapp;
 
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToogle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mDrawerLayout = (DrawerLayout)findViewById(R.id.drawerLayout);
+        mToogle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.app_name, R.string.app_name);
+
+        mDrawerLayout.addDrawerListener(mToogle);
+        mToogle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        if (mToogle.onOptionsItemSelected(item))
+        {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
