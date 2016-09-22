28634e992a0d83ce14695f7774d2c7530ef64796
ekalac1 <elzaa_95@hotmail.com>
2016-09-22 14:56:11 +0200

    API_KEY change

--- MovieApp/app/src/main/java/ba/unsa/etf/rma/elza_kalac/movieapp/ApiClient.java
+++ MovieApp/app/src/main/java/ba/unsa/etf/rma/elza_kalac/movieapp/ApiClient.java
@@ -6,6 +6,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class ApiClient {
 
     public static final String BASE_URL = "http://api.themoviedb.org/3/";
+    public static final String API_KEY = BuildConfig.API_KEY;
     private static Retrofit retrofit = null;
 
 
