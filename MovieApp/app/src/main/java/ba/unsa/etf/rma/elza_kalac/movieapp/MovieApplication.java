e4ea7e01ac38417f1b86ac82b95b8f55186c48ee
Irma Balic <irma.balic@atlantbh.com>
2016-12-08 16:14:04 +0100

    Small refactoring

--- MovieApp/app/src/main/java/ba/unsa/etf/rma/elza_kalac/movieapp/MovieApplication.java
+++ MovieApp/app/src/main/java/ba/unsa/etf/rma/elza_kalac/movieapp/MovieApplication.java
@@ -34,6 +34,7 @@ public class MovieApplication extends Application {
     public static String order = "created_at.desc";
     ApiInterface apiService;
     User account;
+    SharedPreferences settings;
 
     boolean pushM, pushTv;
 
@@ -48,77 +49,24 @@ public class MovieApplication extends Application {
         super.onCreate();
         singleton = this;
         apiService = ApiClient.getClient().create(ApiInterface.class);
-        SharedPreferences settings = getSharedPreferences("account", 0);
-        Calendar calendar = Calendar.getInstance();
-        calendar.setTimeInMillis(System.currentTimeMillis());
-        //  calendar.set(Calendar.HOUR_OF_DAY, 13);
-        // calendar.set(Calendar.MINUTE, 42);
-        Intent intentAlarm = new Intent(this, AlarmReciever.class);
-        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
-        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP,calendar.getTimeInMillis()+1000,AlarmManager.INTERVAL_DAY,  PendingIntent.getBroadcast(this,1,  intentAlarm, PendingIntent.FLAG_UPDATE_CURRENT));
-        if (settings.getBoolean("accInfo", false))
-        {
-            final Account a = new Account();
-            a.setUsername(settings.getString("accUserName", null));
-            a.setSessionId(settings.getString("accSession", null));
-            a.setAccountId(settings.getInt("accId", 0));
-            a.setName(settings.getString("accName", ""));
-            pushM=(settings.getBoolean("accMovie", true));
-            pushTv=(settings.getBoolean("accTv", true));
-            setAccount(a);
-            Call<MoviesListResponse> call=apiService.getFavoritesMovies(a.getAccountId(), ApiClient.API_KEY, a.getSessionId(), order);
-            call.enqueue(new Callback<MoviesListResponse>() {
-                @Override
-                public void onResponse(Call<MoviesListResponse> call, Response<MoviesListResponse> response) {
-                    account.setFavoriteMovies(response.body().getResults());
-                }
-                @Override
-                public void onFailure(Call<MoviesListResponse> call, Throwable t) {
-                }
-            });
-            Call<MoviesListResponse> call1 = apiService.getMoviesWatchList(a.getAccountId(), ApiClient.API_KEY, a.getSessionId(), order);
-            call1.enqueue(new Callback<MoviesListResponse>() {
-                @Override
-                public void onResponse(Call<MoviesListResponse> call, Response<MoviesListResponse> response) {
-                    account.setWatchListMovies(response.body().getResults());
-                }
-                @Override
-                public void onFailure(Call<MoviesListResponse> call, Throwable t) {
+        settings = getSharedPreferences("account", 0);
 
-                }
-            });
-            Call<TvShowResponse> call2 = apiService.getFavoritesTvShows(a.getAccountId(), ApiClient.API_KEY, a.getSessionId(), order);
-            call2.enqueue(new Callback<TvShowResponse>() {
-                @Override
-                public void onResponse(Call<TvShowResponse> call, Response<TvShowResponse> response) {
-                    account.setFavoriteTvShows(response.body().getResults());
-                }
-                @Override
-                public void onFailure(Call<TvShowResponse> call, Throwable t) {
+        setAlarmForPushNotifications();
 
-                }
-            });
-            Call<TvShowResponse> call3 = apiService.getTvShowWatchList(a.getAccountId(), ApiClient.API_KEY, a.getSessionId(), order);
-            call3.enqueue(new Callback<TvShowResponse>() {
-                @Override
-                public void onResponse(Call<TvShowResponse> call, Response<TvShowResponse> response) {
-                    account.setWatchListTvShow(response.body().getResults());
-                }
-                @Override
-                public void onFailure(Call<TvShowResponse> call, Throwable t) {
-
-                }
-            });
+        if (settings.getBoolean("accInfo", false)) {
+            loadAccountInfo();
         }
-        else
+        else {
             setAccount(null);
+        }
     }
+
     public ApiInterface getApiService() {
         return apiService;
     }
 
     public void setAccount(Account a) {
-        if (a!=null)
+        if (a != null)
         {
             User n = new User();
             n.setName(a.getName());
@@ -127,12 +75,86 @@ public class MovieApplication extends Application {
             n.setAccountId(a.getAccountId());
             n.setMoviePushNof(pushM);
             n.setTvShowPushNot(pushTv);
-            account=n;
+            account = n;
         }
-        else account=null;
+        else account = null;
     }
 
     public User getAccount() {
         return account;
+    }
+
+    public void setAlarmForPushNotifications() {
+        // Set alarm for push notifications
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(System.currentTimeMillis());
+        // calendar.set(Calendar.HOUR_OF_DAY, 13);
+        // calendar.set(Calendar.MINUTE, 42);
+        Intent intentAlarm = new Intent(this, AlarmReciever.class);
+        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP,calendar.getTimeInMillis()+1000,AlarmManager.INTERVAL_DAY,  PendingIntent.getBroadcast(this,1,  intentAlarm, PendingIntent.FLAG_UPDATE_CURRENT));
+    }
+
+    public void loadAccountInfo() {
+        // Set account
+        final Account a = new Account();
+        a.setUsername(settings.getString("accUserName", null));
+        a.setSessionId(settings.getString("accSession", null));
+        a.setAccountId(settings.getInt("accId", 0));
+        a.setName(settings.getString("accName", ""));
+        pushM = (settings.getBoolean("accMovie", true));
+        pushTv = (settings.getBoolean("accTv", true));
+        setAccount(a);
+
+        // Get favorite movies
+        Call<MoviesListResponse> call = apiService.getFavoritesMovies(a.getAccountId(), ApiClient.API_KEY, a.getSessionId(), order);
+        call.enqueue(new Callback<MoviesListResponse>() {
+            @Override
+            public void onResponse(Call<MoviesListResponse> call, Response<MoviesListResponse> response) {
+                account.setFavoriteMovies(response.body().getResults());
+            }
+            @Override
+            public void onFailure(Call<MoviesListResponse> call, Throwable t) {
+            }
+        });
+
+        // Get movies in watchlist
+        Call<MoviesListResponse> call1 = apiService.getMoviesWatchList(a.getAccountId(), ApiClient.API_KEY, a.getSessionId(), order);
+        call1.enqueue(new Callback<MoviesListResponse>() {
+            @Override
+            public void onResponse(Call<MoviesListResponse> call, Response<MoviesListResponse> response) {
+                account.setWatchListMovies(response.body().getResults());
+            }
+            @Override
+            public void onFailure(Call<MoviesListResponse> call, Throwable t) {
+
+            }
+        });
+
+        // Get favorite tv shows
+        Call<TvShowResponse> call2 = apiService.getFavoritesTvShows(a.getAccountId(), ApiClient.API_KEY, a.getSessionId(), order);
+        call2.enqueue(new Callback<TvShowResponse>() {
+            @Override
+            public void onResponse(Call<TvShowResponse> call, Response<TvShowResponse> response) {
+                account.setFavoriteTvShows(response.body().getResults());
+            }
+            @Override
+            public void onFailure(Call<TvShowResponse> call, Throwable t) {
+
+            }
+        });
+
+        // Get tv shows in watchlist
+        Call<TvShowResponse> call3 = apiService.getTvShowWatchList(a.getAccountId(), ApiClient.API_KEY, a.getSessionId(), order);
+        call3.enqueue(new Callback<TvShowResponse>() {
+            @Override
+            public void onResponse(Call<TvShowResponse> call, Response<TvShowResponse> response) {
+                account.setWatchListTvShow(response.body().getResults());
+            }
+            @Override
+            public void onFailure(Call<TvShowResponse> call, Throwable t) {
+
+            }
+        });
     }
 }
